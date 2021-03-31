42a8974d71c5b807041f21528e12fb86742daf79
João Gonçalves <joaopedro.jp.4998@gmail.com>
2021-03-31 16:36:16 +0100

    Add context to balance and income expenses

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,8 +1,17 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
+
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <>
       <h4>Expense Tracker</h4>
-      <h2>$0.00</h2>
+      <h2>${total}</h2>
     </>
   );
 };
